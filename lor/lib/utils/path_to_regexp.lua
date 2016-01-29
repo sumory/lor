d7fdd1796c2c13c229c73bd15677d5024d9b0aa4
sumory <sumory.wu@gmail.com>
2016-01-29 18:29:05 +0800

    support path that contains "-"

--- lor/lib/utils/path_to_regexp.lua
+++ lor/lib/utils/path_to_regexp.lua
@@ -16,9 +16,9 @@ end
 function _M.parse_pattern(path, keys, options)
     path = _M.clear_slash(path)
 
-    local new_pattern = sgsub(path, "/:([A-Za-z0-9_]+)", function(m)
+    local new_pattern = sgsub(path, "/:([A-Za-z0-9_-]+)", function(m)
         tappend(keys, m)
-        return "/([A-Za-z0-9_]+)"
+        return "/([A-Za-z0-9_-]+)"
     end)
 
     -- 以*结尾
