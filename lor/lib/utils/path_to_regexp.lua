a556c0287753fb255aa103fc5348a0cbae220dcb
sumory <sumory.wu@gmail.com>
2016-02-02 12:37:14 +0800

    optimize: use ngx.re to replace string.*

--- lor/lib/utils/path_to_regexp.lua
+++ lor/lib/utils/path_to_regexp.lua
@@ -1,15 +1,15 @@
-local sgsub = string.gsub
-local smatch = string.match
-local gmatch = string.gmatch
+local sgsub = ngx.re.gsub
+local smatch = ngx.re.match
 local function tappend(t, v) t[#t+1] = v end
 local debug = require("lor.lib.debug")
+
 
 -- pattern或是uri末尾为n个/，均忽略
 local _M = {}
 
 -- 去除多余的/
 function _M.clear_slash(s)
-    s, _ = sgsub(s, "(/+)", "/")
+    s, _ = sgsub(s, "(/+)", "/", "io")
     return s
 end
 --
@@ -17,14 +17,20 @@ function _M.parse_pattern(path, keys, options)
     path = _M.clear_slash(path)
 
     local new_pattern = sgsub(path, "/:([A-Za-z0-9_-]+)", function(m)
-        tappend(keys, m)
+        if m and type(m) == 'table' then
+            -- for i, v in pairs(m) do
+            -- 	ngx.say(i .. " * ".. v)
+            -- end
+            tappend(keys, m[1])
+        end
+
         return "/([A-Za-z0-9_-]+)"
-    end)
+    end, "io")
 
     -- 以*结尾
-    local all_pattern = sgsub(new_pattern, "/(%*)", function(m)
+    local all_pattern = sgsub(new_pattern, "/[%*]+", function(m)
         return "/(.*)" -- "/(.)"
-    end)
+    end, "io")
     return all_pattern
 end
 
@@ -32,31 +38,44 @@ function _M.parse_path(uri, pattern, keys)
     uri = _M.clear_slash(uri)
 
     local params = {}
-    local match = { smatch(uri, pattern) } -- param values
-    if #match > 0 then -- uri match some route
-    for j = 1, #match do
-        if match[j] then
-            local param_name = keys[j]
-            if param_name then
-                params[param_name] = match[j]
+    local match, err =  smatch(uri, pattern, "io")  -- match is nil or array, param values
+    if match then
+        for j = 1, #match do
+            if match[j] then
+                local param_name = keys[j]
+                if param_name then
+                    params[param_name] = match[j]
+                end
             end
         end
-    end
     else
-        debug("path_to_regexp.lua#parse_path not match", uri, pattern)
+        if err then
+            ngx.log(ngx.ERR, "parse_path error: ", uri, " " , pattern)
+        end
+        return params
     end
+
     return params
 end
 
 function _M.is_match(uri, pattern)
-    debug("path_to_regexp.lua#is_match, uri:", uri, "pattern:", pattern)
-    if not pattern then
-        debug("empty pattern")
+    if not uri or not pattern then
         return false
     end
 
-    local ok = smatch(uri, pattern)
-    if ok then return true else return false end
+    local ok, err = smatch(uri, pattern, "io")
+    if ok then
+        -- for i,v in ipairs(ok) do
+        -- 	ngx.say(#ok , " ", ok[0] .. " " .. i .. " |-> " .. v)
+        -- end
+
+        return true
+    else
+        if err then
+            ngx.log(ngx.ERR, "is_match error: ", uri, " ", pattern, " ",  err)
+        end
+        return false
+    end
 end
 
 
