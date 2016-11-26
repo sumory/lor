ce3a85c4bf413190a21e7490ae76b5bfd26ca16e
sumory <sumory.wu@gmail.com>
2016-11-26 18:24:50 +0800

    update session middleware: - a new param `timeout` to set session timeout, default is 3600 seconds - a new param `refresh_cookie` to control whether to refresh cookie expire or not when request coming, default is true

--- lib/lor/lib/middleware/session.lua
+++ lib/lor/lib/middleware/session.lua
@@ -1,3 +1,6 @@
+local xpcall = xpcall
+local traceback = debug.traceback
+local ngx_time = ngx.time
 local Session = require("resty.session")
 
 -- Mind:
@@ -30,48 +33,83 @@ local Session = require("resty.session")
 --    app:get("/session/destroy", function(req, res, next)
 --        req.session.destroy()
 --    end)
-local session_middleware = function(sessionConfig)
-    sessionConfig = sessionConfig or {}
+local session_middleware = function(config)
+    config = config or {}
+    if config.refresh_cookie ~= false then
+        config.refresh_cookie = true
+    end
+
+    if not config.timeout or type(config.timeout) ~= "number" then
+        config.timeout = 3600 -- default session timeout is 3600 seconds
+    end
+
+    if not config.secret then
+        config.secret = "7su3k78hjqw90fvj480fsdi934j7ery3n59ljf295d"
+    end
+
     return function(req, res, next)
-        -- local config = sessionConfig or {}
+        -- local config = config or {}
         -- config.storage = config.storage or "cookie" -- default is “cookie”
         -- local session = Session.new(config)
         req.session = {
             set = function(key, value)
-                local s = Session.start({
-                    secret = sessionConfig.secret or "7su3k78hjqw90fvj480fsdi934j7ery3n59ljf295d"
+                local s = Session:open({
+                    secret = config.secret
                 })
+
                 s.data[key] = value
+
+                s.cookie.persistent = true
+                s.cookie.lifetime = config.timeout
+                s.expires = ngx_time() + config.timeout
                 s:save()
             end,
             
             update = function() 
-                local s = Session.open({ 
-                    secret = sessionConfig.secret or "7su3k78hjqw90fvj480fsdi934j7ery3n59ljf295d" 
-                }) 
-                s.expire = ngx.time() + s.cookie.lifetime 
+                local s = Session:start({ 
+                    secret = config.secret
+                })
+
+                s.cookie.persistent = true
+                s.expires = ngx_time() + config.timeout
+                s.cookie.lifetime = config.timeout
                 s:save() 
             end,
             
             get = function(key)
-                local s = Session.open({
-                    secret = sessionConfig.secret or "7su3k78hjqw90fvj480fsdi934j7ery3n59ljf295d"
+                local s = Session:open({
+                    secret = config.secret
                 })
-                return s.data[key] or ""
+
+                s.cookie.persistent = true
+                s.cookie.lifetime = config.timeout
+                s.expires = ngx_time() + config.timeout
+                return s.data[key]
             end,
 
             destroy = function()
-                local session = Session.start({
-                    secret = sessionConfig.secret or "7su3k78hjqw90fvj480fsdi934j7ery3n59ljf295d"
+                local s = Session.start({
+                    secret = config.secret
                 })
-                session:destroy()
+                s:destroy()
             end
         }
+
+        local e, ok
+        ok = xpcall(function() 
+            if config and config.refresh_cookie == true then
+                req.session.update()
+            end
+        end, function()
+            e = traceback()
+        end)
+
+        if not ok then
+            ngx.log(ngx.ERR, "[session middleware]refresh cookie error, ", e)
+        end
+
         next()
     end
 end
-
-
-
 
 return session_middleware
