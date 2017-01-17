5b2f12afa45e529682a40367f83bbf1011e4a0b0
sumory <sumory.wu@gmail.com>
2017-01-18 00:54:19 +0800

    update path pattern test case

--- test/path_pattern.test.lua
+++ test/path_pattern.test.lua
@@ -5,77 +5,67 @@ expose("expose modules", function()
     _G.response = require("test.mock_response")
 end)
 
+before_each(function()
+    lor = _G.lor
+    app = lor({
+        debug = false
+    })
+    Request = _G.request
+    Response = _G.response
+    req = Request:new()
+    res = Response:new()
+
+    count = 0
+
+    app:use("/", function(req, res, next)
+        count = 1
+        next()
+    end)
+
+    app:use("/user/", function(req, res, next)
+        count = 2
+        next()
+    end)
+    app:use("/user/:id/view", function(req, res, next)
+        count = 3
+        next()
+    end)
+    app:get("/user/123/view", function(req, res, next)
+        count = 4
+        next()
+    end)
+
+    app:post("/book" , function(req, res, next)
+        count = 5
+        next()
+    end)
+
+    local testRouter = lor:Router() -- 一个新的router，区别于主router
+    testRouter:get("/get", function(req, res, next)
+        count = 6
+        next()
+    end)
+    testRouter:post("/foo/bar", function(req, res, next)
+        count = 7
+        next()
+    end)
+    app:use("/test", testRouter())
+
+    app:erroruse(function(err, req, res, next)
+        count = 999
+    end)
+end)
+
+after_each(function()
+    lor = nil
+    app = nil
+    Request = nil
+    Response = nil
+    req = nil
+    res = nil
+end)
+
 describe("next function usages test", function()
-
-    setup(function()
-    end)
-
-    teardown(function()
-    end)
-
-    before_each(function()
-        lor = _G.lor
-        app = lor({
-            debug = false
-        })
-        Request = _G.request
-        Response = _G.response
-        req = Request:new()
-        res = Response:new()
-
-        count = 0
-
-        app:use("/", function(req, res, next)
-            count = 1
-            next()
-        end)
-
-        app:use("/user/", function(req, res, next)
-            count = 2
-            next()
-        end)
-        app:use("/user/:id/view", function(req, res, next)
-            count = 3
-            next()
-        end)
-        app:get("/user/123/view", function(req, res, next)
-            count = 4
-            next()
-        end)
-
-        app:post("/book" , function(req, res, next)
-            count = 5
-            next()
-        end)
-
-
-        local testRouter = lor:Router() -- 一个新的router，区别于主router
-        testRouter:get("/get", function(req, res, next)
-            count = 6
-            next()
-        end)
-        testRouter:post("/foo/bar", function(req, res, next)
-            count = 7
-            next()
-        end)
-        app:use("/test", testRouter())
-
-
-        app:erroruse(function(err, req, res, next)
-            assert.is_not_nil(err)
-            count = 999
-        end)
-    end)
-
-    after_each(function()
-        lor = nil
-        app = nil
-        Request = nil
-        Response = nil
-        req = nil
-        res = nil
-    end)
-
     it("test case 1", function()
         req.path = "/user/123/view"
         req.method = "get"
@@ -84,27 +74,26 @@ describe("next function usages test", function()
         end)
 
         assert.is.equals(4, count)
-        assert.is.equals("123", req.params.id)
+        assert.is.equals(nil, req.params.id)
     end)
 
-    it("test case 2", function()-- 404 not found
-        req.path = "/user/123/view/" -- 不能匹配app:get("/user/123/view", fn()), 多了一个slash
+    it("test case 2", function() -- route found
+        req.path = "/user/123/view/" -- 匹配app:get("/user/123/view", fn()), 默认配置会trim右边slash
         req.method = "get"
         app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found()) -- 404 not found
+            assert.is_true(req:is_found())
         end)
 
-        assert.is.equals(3, count)
-        assert.is.equals("123", req.params.id)
-
+        assert.is.equals(4, count)
+        assert.is.equals(nil, req.params.id)
     end)
 
     it("test case 3", function()
-        req.path = "/book" -- 不能匹配app:get("/user/123/view", fn()), 多了一个slash
+        req.path = "/book"
         req.method = "get"
         app:handle(req, res)
 
-        assert.is.equals(1, count)
+        assert.is.equals(999, count)
         assert.is_nil( req.params.id)
 
         req.method = "post" -- post match
@@ -117,69 +106,14 @@ describe("next function usages test", function()
     end)
 
     it("test case 4", function()
-        req.path = "/notfound" -- 不能匹配app:get("/user/123/view", fn()), 多了一个slash
+        req.path = "/notfound"
         req.method = "get"
         app:handle(req, res, function(err)
             assert.is_not_true(req:is_found())
             assert.is_nil(err)
         end)
 
-        assert.is.equals(1, count)
-        assert.is_nil( req.params.id)
-    end)
-
-    it("test case 5", function() -- should be `not found`
-        req.path = "/user_abc/123/view" -- 不能匹配app:get("/user/123/view", fn()), 多了一个slash
-        req.method = "get"
-        app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found())
-        end)
-
-        assert.is.equals(1, count)
-        assert.is_nil( req.params.id)
-    end)
-
-    it("test case 6", function() -- should be `not found`
-        req.path = "/user/123/view_mm" -- 不能匹配app:get("/user/123/view", fn()), 多了一个slash
-        req.method = "get"
-        app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found())
-        end)
-
-        assert.is.equals(2, count) -- passed 2 middleware
+        assert.is.equals(999, count)
         assert.is_nil(req.params.id)
-    end)
-
-    it("test case 7", function() -- 404且不能解析参数
-        req.path = "/user/inject/123/view"
-        req.method = "get"
-        app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found())
-        end)
-
-        assert.is.equals(2, count)
-        assert.is_nil(req.params.id)
-    end)
-
-    it("test case 8", function() -- 过了3个middleware，解析了path variable，但是最后匹配不到路由，则为404
-        req.path = "/user/123/view/inject"
-        req.method = "get"
-        app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found())
-        end)
-
-        assert.is.equals(3, count)
-        assert.is.equals("123", req.params.id)
-    end)
-
-    it("test case 9", function() -- 过了3个middleware，解析了path variable，但是最后匹配不到路由，则为404
-        req.path = "/inject/user/123/view"
-        req.method = "get"
-        app:handle(req, res, function(err)
-            assert.is_not_true(req:is_found())
-        end)
-
-        assert.is.equals(1, count)
-        assert.is.equals(nil, req.params.id)
     end)
 end)
